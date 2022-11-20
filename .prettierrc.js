d56ecd50f89a9ff1cd77a6ec8503e2604119f435
Fabian Flores <fabian@fabiandean.dev>
2022-11-20 13:34:30 -0800

    fix: failing screenshots + converted to TS and other misc improvements

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+// prettier config for src folder
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  parser: 'typescript',
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  useTabs: false,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
